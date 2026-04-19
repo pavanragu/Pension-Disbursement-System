f3b8855ae07bbe1dcfb2543d03cf66fd0c539156
Pavan Ragu <pr0391@srmist.edu.in>
2026-04-19 21:25:41 +0530

    Update app_mysql.py

--- app_mysql.py
+++ app_mysql.py
@@ -18,7 +18,7 @@ DB_CONFIG = {
     'user':     os.environ.get('DB_USER'),
     'password': os.environ.get('DB_PASSWORD'),
     'database': os.environ.get('DB_NAME'),
-    'port': int(os.environ.get('DB_PORT', 3306))
+    'port': int(os.environ.get('DB_PORT', 3306)),
     'charset':  'utf8mb4'
 }
 
